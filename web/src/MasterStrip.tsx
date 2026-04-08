d480d20c6a445bc2acb9d75053970f58fb274c08
Yann Hodique <yann.hodique@gmail.com>
2026-04-08 10:04:32 +0200

    fix(web): align master strip fader with channel strip faders

--- web/src/MasterStrip.tsx
+++ web/src/MasterStrip.tsx
@@ -1,15 +1,26 @@
 import { VolumeFader } from "./VolumeFader";
-import css from "./MasterStrip.module.css";
+import { PanKnob } from "./PanKnob";
+import css from "./ChannelStrip.module.css";
+import masterCss from "./MasterStrip.module.css";
 
 interface Props {
   value: number;
   onChange: (value: number) => void;
 }
 
+const noop = () => {};
+
 export function MasterStrip({ value, onChange }: Props) {
   return (
-    <div className={css.strip}>
-      <div className={css.label}>Master</div>
+    <div className={`${css.strip} ${masterCss.override}`}>
+      <div className={css.partNumber}>Master</div>
+      {/* Exact same elements as ChannelStrip, visibility:hidden for alignment */}
+      <div className={masterCss.hidden}>
+        <button className={css.eqButton}>EQ</button>
+        <PanKnob value={64} onChange={noop} />
+        <span className={css.muteLabel}>MUTE</span>
+        <button className={css.muteButton}>M</button>
+      </div>
       <div className={css.faderArea}>
         <VolumeFader value={value} onChange={onChange} />
       </div>
